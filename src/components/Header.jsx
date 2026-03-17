cd78d3a60b2c777dfd5baac6fd78b8a574cf2770
stephanybrazeir0 <stephanybrazeiro@gmail.com>
2026-03-17 20:49:17 -0300

    feat: add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 
+import Dogs from "../assets/dogs.svg?react";
+
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" aria-label="Dogs - Home" className={styles.logo}>
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
